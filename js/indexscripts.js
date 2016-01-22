60aa14b11bc1817f4a6041bd5f612251e3d431d6
Michael <michael.vang.1985@gmail.com>
2016-01-22 12:15:51 -0800

    add submit form to html

--- js/indexscripts.js
+++ js/indexscripts.js
@@ -1,37 +1,45 @@
 
-var testArray = function(input) {
+var output = function(input) {
+
+
 
   var numberArray = [];
 
+
   for (var i = 1; i <= input; i++) {
+    numberArray.push("<li>")
     numberArray.push(i);
 
 
     if (i % 15 === 0) {
       numberArray.pop();
-      numberArray.push("pingpong");
+      numberArray.push("<img src=img/ping.png>","pingpong","<img src=img/pong.png>");
     }
 
     else if (i % 3 === 0) {
       numberArray.pop();
-      numberArray.push("ping");
+      numberArray.push("<img src=img/ping.png>","ping");
     }
 
     else if (i % 5 === 0) {
       numberArray.pop();
-      numberArray.push("pong");
+      numberArray.push("pong","<img src=img/pong.png>");
     }
     console.log(numberArray);
   }
-
+  numberArray = numberArray.join("")
   return numberArray;
+  //
+  // var outstring = numberArray.join("")
+  //
+  // return outstring;
 };
 
 $(document).ready(function(){
 
-  $("form#replaceVowel").submit(function(event) {
-    var string = $("input#string").val();
-    var toPrint = splitString(string);
+  $("form#numberInput").submit(function(event) {
+    var input = $("input#number").val();
+    var toPrint = output(input);
 
     $("#printTo").append("<br><hr>",toPrint,"<br><hr><strong>Good Luck!</strong>");
     $("#hide").hide();

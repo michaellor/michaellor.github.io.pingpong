647298ffd91680e526872732fe2aa01d2ae9ded4
Michael <michael.vang.1985@gmail.com>
2016-01-22 16:34:49 -0800

    add image and image properties

--- js/indexscripts.js
+++ js/indexscripts.js
@@ -1,30 +1,23 @@
 
 var output = function(input) {
 
-
   if(input.match(/^[a-zA-Z]+$/)) {
     return alert("Numbers only!");
   }
 
   var numberArray = [];
 
-
   for (var i = 1; i <= input; i++) {
     numberArray.push("<br>")
     numberArray.push(i);
 
-
     if (i % 15 === 0) {
       numberArray.pop();
       numberArray.push("<img src=img/ping.png>","<strong>PING-PONG!</strong>","<img src=img/pong.png>");
-    }
-
-    else if (i % 3 === 0) {
+    } else if (i % 3 === 0) {
       numberArray.pop();
       numberArray.push("<img src=img/ping.png>","<strong>PING!</strong>");
-    }
-
-    else if (i % 5 === 0) {
+    } else if (i % 5 === 0) {
       numberArray.pop();
       numberArray.push("<strong>PONG!</strong>","<img src=img/pong.png>");
     }
@@ -39,14 +32,13 @@ $(document).ready(function(){
   $("form#numberInput").submit(function(event) {
     var input = $("input#number").val();
     var toPrint = output(input);
+    var scoreCounterOne = (toPrint.match(/PING/g) || []).length;
+    var scoreCounterTwo = (toPrint.match(/PONG/g) || []).length;
 
-    var scoreCounter = (toPrint.match(/PING/g) || []).length;
-
-    console.log(scoreCounter);
-
-
-
-    $("#printTo").hide().append("<br>",toPrint,"<br><br><br>","<form><input class='btn btn-primary' type='button' onClick='history.go(0)' value='Try Again.'></form>").fadeIn(3000);
+    $("#printTo").hide().append("<br><img src=img/babymarioluigi.png id='smallIMG'><br>",toPrint,"<br><br><br>","<form><input class='btn btn-primary' type='button' onClick='history.go(0)' value='Try Again.'></form>").fadeIn(3000);
+      console.log(scoreCounterOne, scoreCounterTwo);
+    $("#printCounterOne").append(scoreCounterOne);
+    $("#printCounterTwo").append(scoreCounterTwo);
     $("#hide").hide();
 
     event.preventDefault();
